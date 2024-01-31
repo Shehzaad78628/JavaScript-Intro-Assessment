a7089c5f6b0126ad08749ae069a2fd2516350064
Shehzaad Nasir <shehzaadnasir786@gmail.com>
2024-01-31 16:31:20 -0500

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert ("My Name is Shehzaad Nasir")
+// alert ("I am 17 years old")
+var firstName = 'Shehzaad';
+var lastName = 'Nasir';
+var age = '17';
+alert("My Name Is" +" "+ firstName +" "+ lastName);
+alert("I am" +" "+ age +" "+ "years old");
+let statement = "I'm having a very fun time in this class";
+console.log(statement);  
